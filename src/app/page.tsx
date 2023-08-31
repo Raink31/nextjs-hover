8368701b6cf69d4a5afea39669106a2f9b9175e0
Yohan Baes <yohan.baes97@gmail.com>
2023-08-31 22:51:25 +0200

    correction des apostrophes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,11 +18,11 @@ export default function Home() {
       WebkitMaskPosition: `${x - size/2}px ${y - size/2}px`,
       WebkitMaskSize: `${size}px`
     }} transition={{type: "tween", ease: "backOut"}}> 
-        <p className='w-[1000px]' onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>A visual designer - with skills that haven't been replaced by A.I (yet) - making good shit only if the paycheck is equally good.</p>
+        <p className='w-[1000px]' onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>A visual designer - with skills that haven&apos;t been replaced by A.I (yet) - making good shit only if the paycheck is equally good.</p>
     </motion.div>
 
     <div className='h-full w-full flex items-center justify-center text-[#afa18f] text-[64px] leading-[66px] cursor-default'>
-      <p className='w-[1000px]'>I'm a <span className='text-[#ec4e39]'>selectively skilled</span> product designer with strong focus on productiong high quality & impactful digital experience.</p>
+        <p className='w-[1000px]'>I&apos;m a <span className='text-[#ec4e39]'>selectively skilled</span> product designer with strong focus on productiong high quality & impactful digital experience.</p>
     </div>
 
     </main>
